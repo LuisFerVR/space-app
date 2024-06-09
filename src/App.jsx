6c7dd5f82124d08fa648f788a30d7c7fc34e0423
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-06-09 13:56:20 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+import GlobalStyles from "./components/GlobalStyles"
+
+const FondoGradiante = styled.div`
+  background: linear-gradient(175deg,#041833 4.16%,#04244f 48%,#154580 96.76%);
+  width:100%;
+  min-height:100vh;
+
+`
+function App() {
+ 
+
+  return (
+    <>
+      <FondoGradiante>
+        <GlobalStyles/>
+      </FondoGradiante>
+    </>
+  )
+}
+
+export default App
