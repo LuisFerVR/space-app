c827ae4901f3b92b8a9286e906f82f333d0c1474
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-06-15 12:41:35 -0600

    Se agregó el banner del título y se garegó el resto de items del sidbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import styled from "styled-components"
 import GlobalStyles from "./components/GlobalStyles"
 import Cabezera from "./components/Cabezera"
 import BarraLateral from "./components/BarraLateral"
+import Banner from "./components/Banner"
+import banner from "./assets/banner.png"
+
 const FondoGradiante = styled.div`
   background: linear-gradient(175deg,#041833 4.16%,#04244f 48%,#154580 96.76%);
   width:100%;
@@ -17,6 +20,7 @@ function App() {
         <GlobalStyles/>
         <Cabezera/>
         <BarraLateral/>
+        <Banner texto="¡Bienvenido a la galeria de fotos más completa del espacio!" backgroundImage={banner}/>
       </FondoGradiante>
     </>
   )
--- src/components/BarraLateral/index.jsx
+++ src/components/BarraLateral/index.jsx
@@ -12,8 +12,11 @@ const BarraLateral = () => {
         <aside>
             <nav>
                 <ListaEstilizda>
-                    <ItemNavegacion iconoActivo="iconos/home-activo.png" iconoInactivo="iconos/home-inactivo.png" activo>HOME</ItemNavegacion>
-                    <ItemNavegacion iconoActivo="iconos/mas-vistas-activo.png" iconoInactivo="iconos/mas-vistas-inactivo.png">MÁS VISTAS</ItemNavegacion>
+                    <ItemNavegacion iconoActivo="iconos/home-activo.png" iconoInactivo="iconos/home-inactivo.png" activo>Inicio</ItemNavegacion>
+                    <ItemNavegacion iconoActivo="iconos/mas-vistas-activo.png" iconoInactivo="iconos/mas-vistas-inactivo.png">Más vistas</ItemNavegacion>
+                    <ItemNavegacion iconoActivo="iconos/me-gusta-activo.png" iconoInactivo="iconos/me-gusta-inactivo.png">Más me gusta</ItemNavegacion>
+                    <ItemNavegacion iconoActivo="iconos/nuevas-activo.png" iconoInactivo="iconos/nuevas-inactivo.png">Nuevas</ItemNavegacion>
+                    <ItemNavegacion iconoActivo="iconos/sorprendeme-activo.png" iconoInactivo="iconos/sorprendeme-inactivo.png">Sorpréndame</ItemNavegacion>
                 </ListaEstilizda>
             </nav>
         </aside>
