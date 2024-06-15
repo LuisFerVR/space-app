d8099d9572ae9d9e851b59c62259712f7aca369e
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-06-15 10:10:53 -0600

    Se estilizo con component style condicional el color de la fuente en el sidbar cuando está activo o no un item

--- src/components/BarraLateral/ItemNavegacion/index.jsx
+++ src/components/BarraLateral/ItemNavegacion/index.jsx
@@ -1,9 +1,19 @@
+import styled from "styled-components"
 
+const ItemsListaEstilizada = styled.li`
+    font-size:24px;
+    margin-bottom: 30px;
+    line-height:28px;
+    display: flex;
+    align-items:center;
+    gap:16px;
+    color:${props=>props.$activo ? "#7B78E5" : "#D9D9D9"};
+`
 const ItemNavegacion = ({children,iconoActivo,iconoInactivo,activo=false}) => {
-    return <>
+    return <ItemsListaEstilizada $activo={activo} >
         <img src={activo ? iconoActivo : iconoInactivo} alt="estado del icono"></img>
         {children}
-    </>
+    </ItemsListaEstilizada>
 
 }
 
